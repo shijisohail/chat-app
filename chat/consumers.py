1af5233ad77d46c59f47ed7bd6c22a6be7fc6f11
Sharjeel <shijisohail786@gmail.com>
2023-11-10 16:42:25 +0500

    misc

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,6 +4,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
